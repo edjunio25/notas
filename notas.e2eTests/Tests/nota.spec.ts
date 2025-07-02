a9469ce2b2374cf75ae83b1b30b5eb787be4331e
Edson <edson.jbp@hotmail.com>
2025-07-02 02:51:09 -0300

    Fixed playwright host

--- notas.e2eTests/Tests/nota.spec.ts
+++ notas.e2eTests/Tests/nota.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Página inicial carrega', async ({ page }) => {
-    await page.goto('https://localhost:65124');
+    await page.goto('https://localhost:4173');
     await expect(page.locator('h1')).toHaveText(/Notas/);
 });
 
--- notas.e2eTests/playwright.config.ts
+++ notas.e2eTests/playwright.config.ts
@@ -5,18 +5,18 @@ export default defineConfig({
     timeout: 30000,
     retries: 1,
     use: {
-        baseURL: 'https://localhost:65124/',
-        headless: false,
+        baseURL: 'https://localhost:4173/',
+        headless: true,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         ignoreHTTPSErrors: true, 
     },
 
     webServer: {
-        command: 'npm run dev',
-        port: 65124,
-        cwd: '../notas.Client', 
+        command: 'npm run preview',
+        port: 4173,
+        cwd: '../notas.client',
         timeout: 30000,
-        reuseExistingServer: !process.env.CI, 
-    },
+        reuseExistingServer: !process.env.CI,
+    }
 });
